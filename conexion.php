01daf0511adf98487838ed30334d125186b0348c
isaac lugo <isaac.e.lugo.g@gmail.com>
2025-07-13 13:41:41 -0400

    add pdo conecdtion

--- conexion.php
+++ conexion.php
@@ -1,6 +1,27 @@
 <?php
 // conexion.php
 
+$host = 'localhost';
+$port = 3306;
+$dbname = 'ORGANIZACION';
+$user = 'root';
+$password = 'thlWgT_6jD/Mka';
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $dsn = "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch (PDOException $e) {
+    error_log("Error de conexión BD: " . $e->getMessage());
+    die("Error de conexión. Por favor, inténtalo más tarde.");
+}
+
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "thlWgT_6jD/Mka";
